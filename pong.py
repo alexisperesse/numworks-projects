c45e5dc2460967ed99aec034f97b3e02385b57e3
alexisperesse <128890598+alexisperesse@users.noreply.github.com>
2023-03-25 19:53:34 +0100

    Update pong.py

--- pong.py
+++ pong.py
@@ -7,12 +7,12 @@ from time import *
 print("play1v1()")
 print("play1vmur()")
 
+def initialisationDuFichierAnnexe():
+  FHighScore=open("highscore2.py","w")
+  FHighScore.write("10")
+
 bluescore=0
 redscore=0
-
-#def initialisationDuFichierÀPart():
-#  FHighScore=open("highscore.py","w")
-#  FHighScore.write("10")
 
 def initialisation():
   global murscore, endloop, y1, y2, xballe, yballe, incrementx, incrementy
@@ -22,35 +22,44 @@ def initialisation():
   y2=100
   xballe=150
   yballe=115
-  Random=[-2,2]
+  Random=[-1,1]
   incrementx=choice(Random)
   incrementy=randint(-1,1)
 
 def raquette1():
   global y1
   if keydown(KEY_SEVEN):
-    fill_rect(5,yl,5,50,color(255,255,255))
-    y1-=4
+    fill_rect(5,y1,5,50,color(255,255,255))
+    y1-=3
     fill_rect(5,y1,5,50,color(255,0,0))
   if keydown(KEY_ZERO):
     fill_rect(5,y1,5,50,color(255,255,255))
-    y1+=4
+    y1+=3
     fill_rect(5,y1,5,50,color(255,0,0))
 
-def raquette2():
+def raquette2(couleur):
   global y2
-  if keydown (KEY_RIGHTPARENTHESIS):
-    fill_rect(310,y2,5,50,color(255,255,255))
-    y2-=4
+  if keydown(KEY_RIGHTPARENTHESIS):
+    if couleur=="blanc":
+      fill_rect(310,y2,5,50,color(255,255,255))
+    elif couleur=="noir":
+      fill_rect(310,y2,5,50,color(255,255,255))
+    y2-=3
     fill_rect(310,y2,5,50,color(0,0,255))
   if keydown (KEY_EXE):
-    fill_rect(310,y2,5,50,color(255,255,255))
-    y2+=4
+    if couleur=="blanc":
+      fill_rect(310,y2,5,50,color(255,255,255))
+    elif couleur=="noir":
+      fill_rect(310,y2,5,50,color(255,255,255))
+    y2+=3
     fill_rect(310,y2,5,50,color(0,0,255))
 
-def balle():
+def balle(couleur):
   global xballe, yballe
-  fill_rect(xballe,yballe,10,10,color(255,255,255))
+   if couleur=="blanc":
+     fill_rect(xballe,yballe,10,10,color(255,255,255))
+   elif couleur=="noir":
+   	 fill_rect(xballe,yballe,10,10,color(0,0,0))
   xballe+=incrementx
   yballe+=incrementy
   fill_rect(xballe,yballe,10,10,color(255,255,0))
@@ -60,15 +69,17 @@ def reboundtest():
   xtest=xballe-5
   ytest=yballe
   for i in range(2):
-    if get_pixel(xtest,ytest)==color(0,0,255):
-      print("bleu repere")
-      incrementx=-5
-      incrementy=randint(-1,1)
-    if get_pixel(xtest,ytest)==color(255,0,0):
-      print("rouge repere")
-      incrementx=5
-      incrementy=randint(-1,1)
-    xtest+=20 
+    for i in range(2):
+      if get_pixel(xtest,ytest)==color(0,0,255):
+        print("bleu repere")
+        incrementx=-3
+        incrementy=randint(-1,1)
+      if get_pixel(xtest,ytest)==color(255,0,0):
+        incrementx=3
+        incrementy=randint(-1,1)
+      ytest+=10
+    xtest+=20
+    ytest=yballe 
 
 def bordertest():
   global incrementy
@@ -81,14 +92,12 @@ def fautetest():
   global bluescore, redscore, endloop
   if xballe<=0:
     bluescore+=1
-    fill_rect(0,0,350,221,color(255,255,255))
     if bluescore<3:
       play1v1()
     elif bluescore==3:
       endloop=1
   if xballe>=350:
     redscore+=1
-    fill_rect(0,0,350,221,color(255,255,255)) 
     if redscore<3:
       play1v1()
     elif redscore==3:
@@ -96,18 +105,19 @@ def fautetest():
       
 def play1v1():
   global bluescore, redscore
+  fill_rect(0,0,350,222,color(255,255,255))
   initialisation()
   draw_string("Bluescore : "+str(bluescore),5,5)
-  draw_string("Redscore : " +str(redscore),120,5)
+  draw_string("Redscore : " +str(redscore),190,5)
   fill_rect(xballe,yballe,10,10,color(255,255,0))
   fill_rect(5,y1,5,50,color(255,0,0))
   fill_rect(310,y2,5,50,color(0,0,255))
   sleep(3)
-  fill_rect(0,0,350,50,color(255,255,255))
+  fill_rect(0,0,350,50,color(0,0,0))
   while endloop==0:
     raquette1()
-    raquette2()
-    balle()
+    raquette2("blanc")
+    balle("blanc")
     reboundtest() 
     bordertest()
     fautetest()
@@ -124,20 +134,23 @@ def play1v1():
       play1v1()
     if keydown(KEY_BACKSPACE):
       return
-      
+          
 def reboundtestmur():
   global incrementx, incrementy, murscore
   xtest=xballe-5
   ytest=yballe
   for i in range(2):
-    if get_pixel(xtest,ytest)==color(0,0,255):
-      if incrementx!=-3:
-        murscore+=1
-      incrementx=-3
-      incrementy=randint(-1,1)
-    elif xtest<=0:
-      incrementx=3
-      inerementy=randint(-1,1)
+    for i in range(2):
+      if get_pixel(xtest,ytest)==color(0,0,255):
+        if incrementx!=-3:
+          murscore+=1
+        incrementx=-2
+        incrementy=randint(-1,1)
+      elif xtest<=0:
+        incrementx=2
+        incrementy=randint(-1,1)
+      ytest+=10
+    ytest=yballe
     xtest+=20
 
 def fautetestmur():
@@ -147,30 +160,32 @@ def fautetestmur():
     endloop=1
 
 def play1vmur():
- # FHighScore=open("highscore.py","r")
- # HighScore=FHighScore.read(2)
- # Fhighscore.close
+  FHighScore=open("highscore2.py","r")
+  HighScore=FHighScore.read(2)
+  FHighScore.close
   initialisation()
+  fill_rect(0,0,350,222,color(0,0,0))
   fill_rect(xballe,yballe,10,10,color(255,255,0))
   fill_rect(310,y2,5,50,color(0,0,255))
   sleep(3)
   while endloop==0:
-    raquette2()
-    balle()
+    raquette2("noir")
+    balle("noir")
     bordertest()
     reboundtestmur()
     fautetestmur()
-  if murscore<10:
+  if murscore>int(HighScore):
+    HighScore=murscore
+    draw_string("Nouveau record: "+str(HighScore),75,100)
+    FHighScore=open("highscore2.py","w")
+    FHighScore.write(str(HighScore))
+    print(HighScore)
+  elif murscore<10:
     draw_string("Ton score est de "+str(murscore)+" (cheh)",45,80)
-#    draw_string("Record actuel: "+str(Highscore),75,150)
-  if murscore>=10:
+    draw_string("Record actuel: "+str(HighScore),75,150)
+  elif murscore>=10:
     draw_string("Ton score est de "+str(murscore)+" bravo !",35,80)
-#    draw_string("Record actuel : "+str(Highscore),75,150)
-#  if murscore>int(Highscore):
-#    Highscore=murscore
-#    draw_string("Nouveau record: "+str(Highscore),75,120)
-#    FHighScore=open("highscore.py","w")
-#    FHighScore.write(str(HighScore))
+    draw_string("Record actuel : "+str(HighScore),75,120)
   while True:
     if keydown(KEY_OK):
       bluescore=0
